febe06d34b71ef7e2a032d9659220347daf40470
Kizito Akhilome <hovkard@gmail.com>
2018-10-20 21:40:21 +0100

    explore jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,10 +2,54 @@
 
 // JSX 
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'This is jsx from src/app.js'
+  React.createElement(
+    'h1',
+    null,
+    'This is a heading'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is jsx from src/app.js'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item 2wo'
+    )
+  )
 );
+
+var secondTemplate = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Kizito Akhilome'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 22'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Lagos'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(secondTemplate, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 // JSX 
-const template = <p>This is jsx from src/app.js</p>;
+const template = (
+  <div>
+    <h1>This is a heading</h1>
+    <p>This is jsx from src/app.js</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item 2wo</li>
+    </ol>
+  </div>
+);
+
+const secondTemplate = (
+  <div>
+    <h1>Kizito Akhilome</h1>
+    <p>Age: 22</p>
+    <p>Location: Lagos</p>
+  </div>
+);
+
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(secondTemplate, appRoot);
