ff6b2059465f8e06d5a6d6583c2bb5a1cb25d680
Kizito Akhilome <hovkard@gmail.com>
2018-10-29 12:58:08 +0100

    install necessary scss loaders and configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
       loader: 'babel-loader',
       test: /\.(js|jsx)$/,
       exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
     }]
   },
   resolve: {
